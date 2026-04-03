8f7995fbaf0a32c59ca320e982960205398c3a9c
Francisco Rodriguez y Barros <franciscoryb1@hotmail.com>
2026-04-03 19:30:41 -0300

    FIX migration.sql, corregi error en migracion sucia por cambios ya aplicados

--- prisma/migrations/20260403221919_add_statement_extras/migration.sql
+++ prisma/migrations/20260403221919_add_statement_extras/migration.sql
@@ -4,28 +4,20 @@
   - A unique constraint covering the columns `[creditCardPurchaseId]` on the table `RecurringExpensePayment` will be added. If there are existing duplicate values, this will fail.
 
 */
--- AlterTable
-ALTER TABLE "RecurringExpensePayment" ADD COLUMN     "creditCardPurchaseId" INTEGER,
-ALTER COLUMN "movementId" DROP NOT NULL;
 
 -- CreateTable
-CREATE TABLE "CreditCardStatementExtra" (
-    "id" SERIAL NOT NULL,
-    "statementId" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
-    "amountCents" INTEGER NOT NULL,
+  CREATE TABLE "CreditCardStatementExtra" (
+      "id" SERIAL NOT NULL,
+      "statementId" INTEGER NOT NULL,
+      "description" TEXT NOT NULL,
+      "amountCents" INTEGER NOT NULL,
 
-    CONSTRAINT "CreditCardStatementExtra_pkey" PRIMARY KEY ("id")
-);
+      CONSTRAINT "CreditCardStatementExtra_pkey" PRIMARY KEY ("id")
+  );
 
--- CreateIndex
-CREATE INDEX "CreditCardStatementExtra_statementId_idx" ON "CreditCardStatementExtra"("statementId");
+  -- CreateIndex
+  CREATE INDEX "CreditCardStatementExtra_statementId_idx" ON "CreditCardStatementExtra"("statementId");
 
--- CreateIndex
-CREATE UNIQUE INDEX "RecurringExpensePayment_creditCardPurchaseId_key" ON "RecurringExpensePayment"("creditCardPurchaseId");
-
--- AddForeignKey
-ALTER TABLE "CreditCardStatementExtra" ADD CONSTRAINT "CreditCardStatementExtra_statementId_fkey" FOREIGN KEY ("statementId") REFERENCES "CreditCardStatement"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "RecurringExpensePayment" ADD CONSTRAINT "RecurringExpensePayment_creditCardPurchaseId_fkey" FOREIGN KEY ("creditCardPurchaseId") REFERENCES "CreditCardPurchase"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+  -- AddForeignKey
+  ALTER TABLE "CreditCardStatementExtra" ADD CONSTRAINT "CreditCardStatementExtra_statementId_fkey"
+  FOREIGN KEY ("statementId") REFERENCES "CreditCardStatement"("id") ON DELETE CASCADE ON UPDATE CASCADE;
